565e98d3d6294651fc570ee561128696eaf3c1fc
Bushra01-Dhaka <humayraanjum87@gmail.com>
2023-12-22 17:04:28 +0600

    Updated Ongoing and Completed Tasks Count and accrding to the status tasks are moved to the required list

--- src/Components/DashCompo/Boxes/Boxes.jsx
+++ src/Components/DashCompo/Boxes/Boxes.jsx
@@ -1,12 +1,15 @@
 import useTasks from "../../../Hooks/useTasks";
 import "./Boxes.css"
 const Boxes = () => {
-  const [task] = useTasks();
+  const [tasks] = useTasks();
+  const completedTasks = tasks.filter(item => item.status === "Completed");
+
+  const ongoingTasks = tasks.filter(item => item.status === "Ongoing");
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-6 px-6 mt-10">
               <div className="card bg-base-100 shadow-xl">
             <div className="card-body">
-              <h2 className="card-title"> <span className="animated-image bg-[#00D7FF] rounded-md text-black p-6">{task?.length}</span></h2>
+              <h2 className="card-title"> <span className="animated-image bg-[#00D7FF] rounded-md text-black p-6">{tasks?.length}</span></h2>
             <div className="py-4">
             <p className=" text-black text-xl font-semibold">Total Tasks</p>
             </div>
@@ -15,7 +18,7 @@ const Boxes = () => {
 
           <div className="card bg-base-100 shadow-xl">
             <div className="card-body">
-              <h2 className="card-title"> <span className="animated-image bg-[#00D7FF] rounded-md text-black p-6">0</span></h2>
+              <h2 className="card-title"> <span className="animated-image bg-[#00D7FF] rounded-md text-black p-6">{ongoingTasks?.length}</span></h2>
             <div className="py-4">
             <p className=" text-black text-xl font-semibold">Ongoing Tasks</p>
             </div>
@@ -24,7 +27,7 @@ const Boxes = () => {
 
           <div className="card bg-base-100 shadow-xl">
             <div className="card-body">
-              <h2 className="card-title"> <span className="animated-image bg-[#00D7FF] rounded-md text-black p-6">0</span></h2>
+              <h2 className="card-title"> <span className="animated-image bg-[#00D7FF] rounded-md text-black p-6">{completedTasks?.length}</span></h2>
             <div className="py-4">
             <p className=" text-black text-xl font-semibold">Completed Tasks</p>
             </div>
--- src/Pages/DashboardPage/MyDash/TasksCreated/TasksCreated.jsx
+++ src/Pages/DashboardPage/MyDash/TasksCreated/TasksCreated.jsx
@@ -6,12 +6,18 @@ const TasksCreated = () => {
     const [tasks] = useTasks();
     console.log(tasks);
 
+    const todoTasks = tasks.filter(item => item.status === "Todo");
+    const completedTasks = tasks.filter(item => item.status === "Completed");
+
+    const ongoingTasks = tasks.filter(item => item.status === "Ongoing");
+    // console.log("Ongoing data", ongoingTasks);
+
     return (
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 py-6 px-4">
             <div className="flex-1 bg-blue-300 rounded shadow-xl">
                 <h1 className="text-center text-2xl font-medium pt-4">Todo-List</h1>
                 {
-                    tasks.map(items => <TodoList
+                    todoTasks.map(items => <TodoList
                     key={items._id}
                     items={items}
                     ></TodoList>)
@@ -20,10 +26,22 @@ const TasksCreated = () => {
 
             <div className="flex-1 bg-purple-300 rounded shadow-xl">
                 <h1 className="text-center text-2xl pt-4 font-medium">Ongoing</h1>
+                {
+                    ongoingTasks.map(items => <TodoList
+                        key={items._id}
+                        items={items}
+                        ></TodoList>)
+                }
             </div>
 
             <div className="flex-1 bg-lime-300 rounded shadow-xl">
                 <h1 className="text-center text-2xl pt-4 font-medium">Completed</h1>
+                {
+                    completedTasks.map(items => <TodoList
+                        key={items._id}
+                        items={items}
+                        ></TodoList>)
+                }
             </div>
         </div>
     );
